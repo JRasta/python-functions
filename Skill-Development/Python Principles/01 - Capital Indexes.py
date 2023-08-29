2852cc7f586446125f60bd48e76bdce4728f3e63
JSmit638 <jsmit638@jci.com>
2023-08-29 14:57:11 +0100

    updated comments

--- Skill-Development/Python Principles/01 - Capital Indexes.py
+++ Skill-Development/Python Principles/01 - Capital Indexes.py
@@ -1,5 +1,7 @@
 """Write a function named capital_indexes. The function takes a single parameter, which is a string.
 Your function should return a list of all the indexes in the string that have capital letters"""
+
+"""Function for above problem"""
 def capital_indexes(x):
     index_lst = []
     for index, letter in enumerate(x):
@@ -7,7 +9,7 @@ def capital_indexes(x):
             index_lst.append(index)
     print(index_lst)
 
-
+"""Tests for above code"""
 capital_indexes("HeLlO")     # [0, 2, 4]
 capital_indexes("BiLLyBoB")  # [0, 2, 3, 5, 7]
 capital_indexes("GoodBYE")   # [0, 4, 5, 6]
