d5b7beefbe00d63deabc1d2bbfadbc62f44adfd7
jonathan <jonathan.smith@inbotiqa.com>
2023-09-14 12:14:41 +0100

    refactored code

--- tKinter/exercises/05 - basic_login_form.py
+++ tKinter/exercises/05 - basic_login_form.py
@@ -1,12 +1,11 @@
+""" A simple Login screen
+Author: Code First with Hala
+Date: 2023
+Reason: Educational
+"""
+
 from tkinter import *
 from tkinter import messagebox
-
-# creating a root widget (window)
-root = Tk()
-root.title('Login Form')
-root.iconbitmap('..\\misc\\tech.ico')  # add user defined icon
-root.geometry('800x500')
-root.configure(bg='#333333')
 
 def login():
     username = 'johnsmith'
@@ -18,29 +17,37 @@ def login():
         messagebox.showerror(title='Login Failed', message='User or Password is incorrect!!')
 
 
-# add a frame for responsive GUI
+# creating a root widget (window)
+root = Tk()
+root.title('Login Form')
+root.iconbitmap('..\\misc\\tech.ico')  # add user defined icon
+root.geometry('800x500')
+root.configure(bg='#333333')
+
+# create a frame for responsive GUI
 frame = Frame(root, bg='#333333')
 
-# create widgets
+# create & place widgets
 login_lbl = Label(frame, text='Login', bg='#333333', fg='#ff0000', font=('Arial', 30))
-username_lbl = Label(frame, text='Username', bg='#333333', fg='#ffffff', font=('Arial', 16))
-username_input = Entry(frame, width='20', borderwidth='2', font=('Arial', 16))
-password_lbl = Label(frame, text='Password', bg='#333333', fg='#ffffff', font=('Arial', 16))
-password_input = Entry(frame, width='20', borderwidth='2', show='*', font=('Arial', 16))
-login_btn = Button(frame, text='Login', bg='#ff0000', fg='#ffffff', font=('Arial', 16), command=lambda: login())
-
-# place widgets on root widget
 login_lbl.grid(row=0, column=0, columnspan=2, sticky='news', pady=40)
+
+username_lbl = Label(frame, text='Username', bg='#333333', fg='#ffffff', font=('Arial', 16))
 username_lbl.grid(row=1, column=0)
+
+username_input = Entry(frame, width='20', borderwidth='2', font=('Arial', 16))
 username_input.grid(row=1, column=1, pady=20)
+
+password_lbl = Label(frame, text='Password', bg='#333333', fg='#ffffff', font=('Arial', 16))
 password_lbl.grid(row=2, column=0)
+
+password_input = Entry(frame, width='20', borderwidth='2', show='*', font=('Arial', 16))
 password_input.grid(row=2, column=1, pady=20)
+
+login_btn = Button(frame, text='Login', bg='#ff0000', fg='#ffffff', font=('Arial', 16), command=lambda: login())
 login_btn.grid(row=3, column=0, columnspan=2, pady=30)
 
-# this allows the responsive
-# centers in the root widget
+# show frame
 frame.pack()
-
 
 # create a mainloop to run the root widget
 root.mainloop()
