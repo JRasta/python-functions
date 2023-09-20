b4a3d9f66e6ce5d255f46b89f2d7b4e7c00010f6
JSmit638 <jsmit638@jci.com>
2023-09-20 10:50:31 +0100

    general add

---
+++ Skill-Development/Python Principles/02 - Middle letter.py
@@ -0,0 +1,22 @@
+""" Write a function named mid that takes a string as its parameter. Your function should extract and return the
+middle letter. If there is no middle letter, your function should return the empty string"""
+
+""" Function for above problem"""
+def mid(x):
+    temp = len(x)
+    if temp % 2 != 0:
+        len_temp = temp % 2
+        result = x[len_temp]
+    else:
+        result = "String has an even letter count..."
+    print(result)
+
+""" Tests for above code"""
+mid("abc")         # "b"
+mid("aaaa")        # ""
+mid("fifth")       # "f"
+mid("bbgghh")      # ""
+mid("abcdefg")     # "d"
+mid("ppiiooll")    # ""
+mid("pilopilog")   # "p"
+mid("pilopiloga")  # ""
