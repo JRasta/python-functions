e2db90a2b2ff36d45d7b77f92d505af2a537d588
jonathan <jonathan.smith@inbotiqa.com>
2023-12-14 14:03:44 +0000

    add poor example

---
+++ beginner-projects/unicode-characters.py
@@ -0,0 +1,26 @@
+print("Unicode -- ASCII Punctuation + Symbols\n")
+print("Exclamation mark : \u0021")
+print("Quotation mark   : \u0022")
+print("Hash tag         : \u0023")
+print("Dollar sign      : \u0024")
+print("Percent sign     : \u0025")  # 5
+print("Ampersand        : \u0026")
+print("Apostrophe       : \u0027")
+print("Left parenthesis : \u0028")
+print("Right parenthesis: \u0029")
+print("Asterisk         : \u002A")  # 10
+print("Plus sign        : \u002B")
+print("Comma            : \u002C")
+print("Hyphen-minus     : \u002D")
+print("Full stop        : \u002E")
+print("Slash            : \u002F")  # 15
+print("Colon            : \u003A")
+print("Semicolon        : \u003B")
+print("Less-than sign   : \u003C")
+print("Equal sign       : \u003D")
+print("Greater-than sign: \u003E")  # 20
+print("Question mark    : \u003F")
+print("At sign          : \u0040")
+print("Back slash       : \u005C")
+print("Circumflex Acc   : \u005E")
+print("Tilde            : \u007E")  # 25
